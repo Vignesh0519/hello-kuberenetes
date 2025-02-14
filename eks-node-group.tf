593ba9e58625570a05c3b6f27500bff625a718e2
Vignesh0519 <Vigneshmesh8680@gmail.com>
2025-02-14 14:22:40 +0530

    workflow files modified

--- eks-node-group.tf
+++ eks-node-group.tf
@@ -1,20 +1,20 @@
-resource "aws_eks_node_group" "eks_nodes" {
-  cluster_name  = aws_eks_cluster.eks_cluster.name
-  node_role_arn = aws_iam_role.eks_worker_role.arn
+# resource "aws_eks_node_group" "eks_nodes" {
+#   cluster_name  = aws_eks_cluster.eks_cluster.name
+#   node_role_arn = aws_iam_role.eks_worker_role.arn
 
-  subnet_ids = [aws_subnet.private_subnet.id]
+#   subnet_ids = [aws_subnet.private_subnet.id]
 
-  scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
-  }
+#   scaling_config {
+#     desired_size = 2
+#     min_size     = 1
+#     max_size     = 3
+#   }
 
-  instance_types = ["t3.medium"]
-  disk_size      = 20
+#   instance_types = ["t3.medium"]
+#   disk_size      = 20
 
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_ecr_readonly,
-    aws_iam_role_policy_attachment.eks_cni_policy
-  ]
-}
+#   depends_on = [
+#     aws_iam_role_policy_attachment.eks_ecr_readonly,
+#     aws_iam_role_policy_attachment.eks_cni_policy
+#   ]
+# }
--- iam-roles.tf
+++ iam-roles.tf
@@ -50,3 +50,43 @@ resource "aws_iam_role_policy_attachment" "eks_vpc_policy_attachment" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
+
+
+resource "aws_iam_role" "code_deploy_role" {
+  name = "CodeDeployRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_policy_attachment" {
+  role       = aws_iam_role.code_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
+resource "aws_iam_role" "eks_role" {
+  name = "eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
